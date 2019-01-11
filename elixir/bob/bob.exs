1689c253c7f23cc8bf8776b58b156a31f2d6d2be
Lexin Gong <gonglexin@gmail.com>
2019-01-11 17:01:18 +0800

    Upgrade to latest Bob exercism

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,11 +1,17 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.trim(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.match?(input, ~r/\pL/) && (String.upcase(input) == input) -> "Whoa, chill out!"
-      String.ends_with?(input, "!") -> "Whatever."
-      true -> "Whatever."
+      String.trim(input) == "" ->
+        "Fine. Be that way!"
+
+      input =~ ~r/\p{Lu}/ && String.upcase(input) == input ->
+        if input =~ ~r/\?$/, do: "Calm down, I know what I'm doing!", else: "Whoa, chill out!"
+
+      String.ends_with?(input, "?") ->
+        "Sure."
+
+      true ->
+        "Whatever."
     end
   end
 end
